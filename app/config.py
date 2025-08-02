3620a8d605cbe13f76f3f6fb66ad2c0e74e90407
amnotwallas <walterardev@gmail.com>
2025-08-02 12:59:49 -0600

    docs(app): Add comments in config.py

--- app/config.py
+++ app/config.py
@@ -2,6 +2,6 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
+# Configuration for the application
 DOWNLOAD_DIR = os.getenv("DOWNLOAD_DIR", "./downloads")
 QUALITY = os.getenv("YT_QUALITY", "192")
