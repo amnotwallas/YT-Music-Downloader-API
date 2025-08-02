a7d586a371184e1573891fa1ce4c3c36d9e9b4ce
amnotwallas <walterardev@gmail.com>
2025-08-02 13:01:03 -0600

    docs(app): Add comments in models.py

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel
 from typing import List
 
+# This model represents a request to download multiple songs
+# It expects a list of song titles or identifiers
 class CancionesRequest(BaseModel):
     canciones: List[str]
